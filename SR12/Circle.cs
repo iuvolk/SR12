869b7fbf3ba3f6e8bacb166071c61b217620280b
Юлия Волкова <volktul@gmail.com>
2022-11-17 22:47:33 +0300

    Добавьте файлы проекта.

---
+++ SR12/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR12
+{
+    static class Circle
+    {
+        static public double GetSquare(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public double GetLenght(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public void CheckCoordinates(double x0, double y0, double x, double y, double r, out bool a)
+        {
+
+            if (Math.Sqrt(Math.Pow(x - x0, 2) + Math.Pow(y - y0, 2)) > r)
+            {
+                a = false;
+            }
+            else
+            {
+                a = true;
+            }
+
+
+
+        }
+    }
+}
---
+++ SR12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты центра окружности x0, y0");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки x, y");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+
+            double s = Circle.GetSquare(r);
+            double l = Circle.GetLenght(r);
+
+            bool a;
+            Circle.CheckCoordinates(x0, y0, x, y, r, out a);
+
+            Console.WriteLine($"Площадь круга {s} \nДлина окружности {l} \nПринадлежность точки окружности {a}");
+            Console.ReadKey();
+        }
+    }
+}
